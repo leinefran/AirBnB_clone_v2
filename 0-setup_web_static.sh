30794b9c9e68b0de2d2b45cca424dda3a7bf1219
leinefran <penny.leine@gmail.com>
2019-01-25 22:45:20 +0000

    Added: nginx and emacs installation command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 # a Bash script that sets up your web servers for the deployment of web_static
 
-mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "I AM A FAKE HTML DOC" > /data/web_static/releases/test/index.html
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install nginx
+sudo apt-get -y install emacs
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo echo "I AM A FAKE HTML DOC" > /data/web_static/releases/test/index.html
 
-chown -R ubuntu:ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
 
 file="/etc/nginx/sites-available/default"
 
-sed -i '/^\tserver_name localhost;/a\\n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;}\n' $file
+sudo sed -i '/^\tserver_name localhost;/a\\n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;}\n' $file
 
-service nginx restart
+sudo service nginx restart
