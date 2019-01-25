550c73aef06be99e5d4747393d906ebcc4b13515
leinefran <penny.leine@gmail.com>
2019-01-25 22:31:35 +0000

    a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 # a Bash script that sets up your web servers for the deployment of web_static
 
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "I AM A FAKE HTML DOC" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+file="/etc/nginx/sites-available/default"
+
+sed -i '/^\tserver_name localhost;/a\\n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;}\n' $file
+
+service nginx restart
