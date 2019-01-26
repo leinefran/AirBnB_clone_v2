293c0083a842ccf1cb23d999522f0779e2d835ea
leinefran <penny.leine@gmail.com>
2019-01-26 02:02:34 +0000

    Added: import modules, Changed: variable name format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # a Fabric script that generates a .tgz archive
 
-from fabric.api import sudo, run, hosts, env, local
-import datetime
+from fabric.api import *
+from datetime import datetime
 
 def do_pack():
-local("mkdir ./versions")
+    local("mkdir -p ./versions")
 
     try:
-        name="web_static_'+%Y-%m-%dT%H:%M:%S'"
-        local("tar -zcvf $name.tar.gz ./web_static")
-        result = local("mv $name.tar.gz ./versions")
+        name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+        local("tar -zcvf " + name + " ./web_static")
+        result = local("mv " + name + " ./versions")
         return(result)
     except:
         return(None)
