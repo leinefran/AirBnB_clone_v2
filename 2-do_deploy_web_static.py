4b293ec88769e93089d8d0eee006613339826358
leinefran <penny.leine@gmail.com>
2019-01-27 23:49:42 +0000

    Added: try and except

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,29 +13,37 @@ env.password = "betty"
 @task
 def do_deploy(archive_path):
     '''a Fabric script that distributes an archive to your web servers'''
-    if (! archive_path):
+    if (!archive_path):
         return (False)
 
-    # upload a tar archive:
-    with cd("/tmp"):
-        upload = put("/home/vagrant/Holberton/AirBnB_clone_v2/versions/" +
-                     "web_static_20190127003308.tgz")
+    try:
 
-    # Verify the upload:
-    upload.succeeded
+        # upload tar archive:
+        with cd("/tmp"):
+            upload = put("/home/vagrant/Holberton/AirBnB_clone_v2/versions/" +
+                         "web_static_20190127003308.tgz")
 
-    # Extract the contents of a tar archive:
-    content = run("tar xzvf /tmp/web_static_20190127003308.tgz")
+        # Verify the upload:
+        upload.succeeded
 
-    # Move contents to a different folder:
-    run("mv" + content + "/data/web_static/releases/web_static_20190127003308")
+        # Extract the contents of a tar archive:
+        content = run("tar xzvf /tmp/web_static_20190127003308.tgz")
 
-    # Delete the archive:
-    run("rm /tmp/web_static_20190127003308.tgz")
+        # Move contents to a different folder:
+        run("mv" + content + "/data/web_static/releases/" +
+            "web_static_20190127003308")
 
-    # Delete a symbolic link:
-    run("rm /data/web_static/current")
+        # Delete the archive:
+        run("rm /tmp/web_static_20190127003308.tgz")
 
-    # Create a new symbolic link:
-    file_path = "/data/web_static/releases/web_static_20190127003308"
-    run("ln -sf" + file_path + "/data/web_static/current")
+        # Delete a symbolic link:
+        run("rm /data/web_static/current")
+
+        # Create a new symbolic link:
+        file_path = "/data/web_static/releases/web_static_20190127003308"
+        run("ln -sf" + file_path + "/data/web_static/current")
+
+        return (True)
+
+    except:
+        return (False)
