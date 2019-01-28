8a7e17ed61de8772e7674cb08f32ea85376335c2
leinefran <penny.leine@gmail.com>
2019-01-28 22:03:59 +0000

    Added: do_pack script, and variable filename. Changed: all commands

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,12 +9,28 @@ env.hosts = ['35.190.162.122', '35.237.116.99']
 env.user = 'ubuntu'
 env.password = "betty"
 
+@task
+def do_pack():
+    '''a Fabric script that generates a .tgz archive'''
+    local("mkdir -p ./versions")
+
+    try:
+        name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+        local("tar -zcvf " + name + " ./web_static")
+        result = local("mv " + name + " ./versions")
+        return(result)
+    except:
+        return(None)
 
 @task
 def do_deploy(archive_path):
     '''a Fabric script that distributes an archive to your web servers'''
     if not archive_path:
         return (False)
+
+    # using the 'split' method to extract only the file name w/o the extension;
+    # 'web_static_20170315003959'
+    filename = archive_path.split("/")[1][:-4]
 
     try:
         # upload tar archive:
@@ -24,21 +40,20 @@ def do_deploy(archive_path):
         upload.succeeded
 
         # Extract the contents of a tar archive:
-        content = run("tar xzvf /tmp/web_static_20190127003308.tgz")
+        content = run("tar xzvf" + filename + ".tgz")
 
         # Move contents to a different folder:
-        run("mv" + content + "/data/web_static/releases/" +
-            "web_static_20190127003308")
+        cd("/tmp/")
+        file_path = run("mv " + content + " /data/web_static/releases/"
 
         # Delete the archive:
-        run("rm /tmp/web_static_20190127003308.tgz")
+        run("rm -rf /tmp/" + filename + ".tgz")
 
         # Delete a symbolic link:
-        run("rm /data/web_static/current")
+        run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link:
-        file_path = "/data/web_static/releases/web_static_20190127003308"
-        run("ln -sf" + file_path + "/data/web_static/current")
+        run("ln -sf " + file_path + " /data/web_static/current")
 
         print("It worked!")
         return (True)
