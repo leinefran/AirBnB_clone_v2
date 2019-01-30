d74acc839dc97e87194b38d18b9e0113dff7e3ee
leinefran <penny.leine@gmail.com>
2019-01-30 20:29:28 +0000

    a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Flask Module'''
+
+
+from flask import Flask
+
+
+web_app = Flask(__name__)
+
+@web_app.route('/', strict_slashes = False)
+def hello():
+    ''' return welcoming str '''
+    return 'Hello, HBNB!'
+
+if __name__ == '__main__':
+
+    app.env = 'development'
+    web_app.run(host='0.0.0.0', port=5000)
