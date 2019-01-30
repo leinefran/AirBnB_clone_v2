3219c371dbd60c2f5b0d53a150b2fb408b078a4c
leinefran <penny.leine@gmail.com>
2019-01-30 21:13:05 +0000

    Changed: name from web_app to app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,15 +5,15 @@
 from flask import Flask
 
 
-web_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@web_app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     ''' return welcoming str '''
     return 'Hello, HBNB!'
 
 if __name__ == '__main__':
 
-    web_app.env = 'development'
-    web_app.run(host='0.0.0.0', port=5000)
+    app.env = 'development'
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,15 +5,15 @@
 from flask import Flask
 
 
-web_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@web_app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     ''' return welcoming str '''
     return 'Hello, HBNB!'
 
-@web_app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def display():
     ''' returns HBNB '''
     return 'HBNB'
@@ -21,5 +21,5 @@ def display():
 
 if __name__ == '__main__':
 
-    web_app.env = 'development'
-    web_app.run(host='0.0.0.0', port=5000)
+    app.env = 'development'
+    app.run(host='0.0.0.0', port=5000)
