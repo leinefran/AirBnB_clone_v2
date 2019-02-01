0dfa3e8026c03c8848f624188049135cc7d4015c
leinefran <penny.leine@gmail.com>
2019-02-01 22:01:52 +0000

    deleted: env variable

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 ''' Flask Module'''
-
-
 from flask import Flask
 
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     ''' return welcoming str '''
     return 'Hello, HBNB!'
 
 if __name__ == '__main__':
-
-    app.env = 'development'
     app.run(host='0.0.0.0', port=5000)
