ec7d14e30bcc1617415ab44b5ae6c0b8696e8eeb
leinefran <penny.leine@gmail.com>
2019-02-01 22:18:41 +0000

    Fixed: last app.route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,34 +6,33 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     ''' return welcoming str '''
     return 'Hello, HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def display_HBNB():
     ''' returns HBNB '''
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def display_C(text=None):
     ''' return C and the value of the variable text '''
     return 'C {}'.format(text).replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python')
+@app.route('/python/<text>')
 def python(text="is cool"):
     ''' return Python and the value of the variable text '''
-    return 'C {}'.format(text).replace('_', ' ')
+    return 'Python {}'.format(text).replace('_', ' ')
 
 
 if __name__ == '__main__':
-
-    app.env = 'development'
     app.run(host='0.0.0.0', port=5000)
